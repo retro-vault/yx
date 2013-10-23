7c353c3fd7e5d80c83a97c6241aa1360ad64638a
Tomaz Stih <tstih@yahoo.com>
2013-10-23 10:15:15 +0100

    Made buddy compilable.

--- buddy/buddy.c
+++ buddy/buddy.c
@@ -4,20 +4,7 @@
  *
  *	tomaz stih mon oct 14 2013
  */
-#include <sdl.h>
  
 int main(int argc, char *argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window *window = SDL_CreateWindow(
-        "Zx Spectrum 48k",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        255, 192, 0
-    );
-
-    SDL_Delay(3000);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+	return 0;
 }
