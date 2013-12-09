c5bebe04d7415148f2e325fe167b53626f042318
Tomaz Stih <tstih@yahoo.com>
2013-12-09 19:46:12 +0000

    Template app and buddy now use sdcc/linux.

--- buddy/buddy.c
+++ buddy/buddy.c
@@ -12,26 +12,9 @@ void harvest_events() {
 
 int main(int argn,char **argv)
 {
-	byte i;
-	rect_t rectangle;
-
-	graphics_t* g=graphics_create(NULL,0);
-
-	byte midx=256/2;
-	byte midy=192/2;
-
-	for (i=0; i<96; i=i+4)
-		graphics_draw_circle(g, midx, midy, i);
-
-	rectangle.x=10;
-	rectangle.y=10;
-	rectangle.w=100;
-	rectangle.h=50;
-	graphics_draw_rect(g, &rectangle);
-
-	harvest_events();
-	
-	graphics_destroy(g);
+	char* screen=16384;
+	int len=6912;
+	while(len--) *screen++=0x55;
 
 	return 0;
 }
--- buddy/message.c
+++ buddy/message.c
@@ -8,7 +8,10 @@
 
 result message_send(window_t *wnd, byte id, word param1, word param2) {
 	/* call wnd_proc directly */
+
+/*
 	wnd->wnd_proc(id, param1, param2);
+*/
 }
 
 result message_post(window_t *wnd, byte id, word param1, word param2) {
