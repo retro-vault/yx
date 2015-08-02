e18c45c69d22d791ca00f81609bb10167fb8337b
Tomaz Stih <tstih@yahoo.com>
2015-08-02 17:53:28 +0100

    template refreshed

---
+++ apps/template/app.c
@@ -0,0 +1,15 @@
+/*
+ *	app.c
+ *	backbone of yeah application
+ *
+ *	tomaz stih thu apr 16 2015
+ */
+#include "app.h"
+
+
+void main() {
+
+	unsigned char *ptr=16384;
+	(*ptr)=0xff;
+	
+}
---
+++ apps/template/app.h
@@ -0,0 +1,12 @@
+/*
+ *	app.h
+ *	application
+ *
+ *	tomaz stih wed apr 29 2015
+ */
+#ifndef _APP_H
+#define _APP_H
+
+
+
+#endif /* _APP_H */
