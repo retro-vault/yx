162f09c2dcbae81a20cb606a64a3309a84e153f3
TurBoss <j.l.toledano.l@gmail.com>
2020-02-08 03:52:03 +0100

    recover string header file from git commits

--- os/ram/string.h
+++ os/ram/string.h
@@ -1,28 +1,14 @@
 /*
- *	string.c
- *	string functions
+ *	string.h
+ *	string functions header
  *
  *	tomaz stih wed apr 10 2013
  */
-#include "yx.h"
+#ifndef _STRING_H
+#define _STRING_H
 
-/* compare two strings */
-byte strcmp(string s1, string s2)
-{
-	while((*s1 && *s2) && (*s1++ == *s2++));
-	return *(--s1) - *(--s2);
-}
+extern byte strcmp(string s1, string s2);
+extern void strcpy(string dest, string src);
+extern word strlen(char *str);
 
-/* copy string to destination strings */
-void strcpy(string dest, string src) {
-	while (*src) (*dest++ = *src++);
-	(*dest)=0; /* terminate string */
-}
-
-/* calc string len */
-word strlen(char *str)
-{
-	const char *s;
-        for (s = str; *s; ++s) /* do nothing */;
-        return (s - str);
-}
+#endif /* string.h */
